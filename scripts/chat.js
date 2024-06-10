767871fe568e316f26ec7535b5d1b363284c17b2
GifftyCode <ulokangozi@yahoo.com>
2024-06-10 21:29:05 +0100

    adding new chat documents

---
+++ scripts/chat.js
@@ -0,0 +1,25 @@
+// adding new chat documents
+// setting up a real-time listener to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection('chats');
+  }
+  async addChat(message) {
+    const now = new Date();
+    // format a chat object
+    const chat = {
+      message,
+      room: this.room,
+      usernaname: this.username,
+      created_at: firebase.firestore.Timestamp.fromDate(now),
+    };
+    // save the chat document
+    const response = await this.chats.add(chat);
+    return response;
+  }
+}
