787233311a842060acb701e51e0ce8aa9f50995f
GifftyCode <ulokangozi@yahoo.com>
2024-06-10 22:00:15 +0100

    setting up a real-time listener to get new chats

--- scripts/chat.js
+++ scripts/chat.js
@@ -22,4 +22,20 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+
+  // setting up a real-time listener to get new chats
+  getChat(callback) {
+    this.chats.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'added') {
+          callback(change.doc.data());
+        }
+      });
+    });
+  }
 }
+
+const chatroom = new Chatroom('general', 'Chantel');
+chatroom.getChat((data) => {
+  console.log(data);
+});
