c4b3d7757f8311d9e6c473841b155e73d15ebf2f
GifftyCode <ulokangozi@yahoo.com>
2024-06-11 21:39:12 +0100

    formating the dates

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,11 +7,16 @@ class ChatUI {
   }
   render(data) {
     // Responsible for creating an html template for each chat doc we get bk and rendering it to the dom.
+    const when = dateFns.distanceInWordsToNow(
+      //A fxn in dateFns that converts dates to words and it takes 2 arg.
+      data.created_at.toDate(), //first arg - date object
+      { addSuffix: true } // second arg. which is optional and add 'ago' to the date
+    );
     const html = `
     <li class='list-group-item'>
         <span class='username'>${data.username}</span>
         <span class='message'>${data.message}</span>
-        <div class='time'>${data.created_at.toDate()}</div>
+        <div class='time'>${when}</div>
     </li>
     `;
     this.list.innerHTML += html;
