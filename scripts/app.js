b8133301375ebbe79d03d86726d3ed7d61ba231a
GifftyCode <ulokangozi@yahoo.com>
2024-06-11 22:24:36 +0100

    updating username

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 // add a new chat
 newChatForm.addEventListener('submit', (e) => {
@@ -10,6 +12,19 @@ newChatForm.addEventListener('submit', (e) => {
     .addChat(message)
     .then(() => newChatForm.reset())
     .catch((err) => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => (updateMssg.innerText = ''), 3000);
 });
 
 // Class instances
